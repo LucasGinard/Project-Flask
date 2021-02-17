7f50718668b105f53d265bce9b79c62ad2692e48
Lucas Ginard <57275367+LucasGinard@users.noreply.github.com>
2021-02-16 21:09:06 -0300

    initial commit

---
+++ schema.py
@@ -0,0 +1,24 @@
+instructions = [
+    'SET FOREIGN_KET_CHECKS=0',
+    'DROP TABLE IF EXISTS todo',
+    'DROP TABLE IF EXISTS user',
+    'SET FOREIGN_KET_CHECKS=1',
+    """
+        CREATE TABLE user(
+            id INT PRIMARY KEY AUTO_INCREMENT,
+            username VARCHAR(200) UNIQUE NOT NULL,
+            password VARCHAR(100) NOT NULL
+        )
+    """,
+    """
+        CREATE TABLE TODO(
+            id INT PRIMARY KEY AUTO_INCREMENT,
+            created_by INT NOT NULL,
+            create_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+            description TEXT NOT NULL,
+            completed BOOLEAN NOT NULL,
+            FOREIGN KEY (created_by) REFERENCE user (id)
+        );
+    """
+
+]
